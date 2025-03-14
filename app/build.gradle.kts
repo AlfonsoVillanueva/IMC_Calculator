24df6fc5af95a37592b3a38acf8c5301c0e6485c
AlfonsoVillanueva <148511018+AlfonsoVillanueva@users.noreply.github.com>
2025-03-13 23:05:02 -0600

    ``` feat: Enhance IMC Calculator UI and add dependencies
    This commit introduces UI enhancements and integrates new dependencies to the IMC Calculator app.
    
    - Updates Kotlin version
    ``````
     to `2.1.0`.
    - Adds Coil libraries for image loading (`coil`, `coil-network-okhttp`, `coil-gif`).
    - Adds Lottie for animations (`com.airbnb.android:lottie`).
    - Includes `exo_2_thin` font for styling.
    -
    ``````
     Introduces preloaded fonts support.
    - Updates app layout (`activity_main.xml`) with UI components:
      - Adds title TextView (`tv_Tittle`) for the app name.
      - Adds instruction TextView (`tv_intrucyion`) for user guidance.
      - Adds EditTexts (`et_altura`, `et_peso`) for user input.
      - Adds button (`btn_calcular`) for calculations.
      - Includes an image view (`imageView`) for visual enhancement.
    - Updates string resources (`strings.xml`) with:
      - Calculation labels.
      - Input field
    ``````
     hints.
    - Adds `font_certs.xml` for managing font certificates.
    - Updates Gradle plugins versions.
    - Add `original_surfer.ttf` as font resource.
    ```

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("io.coil-kt.coil3:coil:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+    implementation("io.coil-kt.coil3:coil-gif:3.1.0")
+    implementation ("com.airbnb.android:lottie:6.6.3")
 }
