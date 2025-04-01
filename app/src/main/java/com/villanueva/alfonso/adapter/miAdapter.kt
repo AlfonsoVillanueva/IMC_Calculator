39b21f58d0f91034b6fc5be783a91733ae3fbdee
AlfonsoVillanueva <148511018+AlfonsoVillanueva@users.noreply.github.com>
2025-04-01 09:24:42 -0600

    dsd

---
+++ app/src/main/java/com/villanueva/alfonso/adapter/miAdapter.kt
@@ -0,0 +1,11 @@
+package com.villanueva.alfonso.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.villanueva.alfonso.RecyclerViewTest
+import com.villanueva.alfonso.data.Gato
+
+class miAdapter (private val dataSet:Array<Gato>): RecyclerView.Adapter<miAdapter.ViewHolder> {
+
+    inner class ViewHolder
+
+}
---
+++ app/src/main/java/com/villanueva/alfonso/data/Gato.kt
@@ -0,0 +1,3 @@
+package com.villanueva.alfonso.data
+
+data class Gato(val imagen:String,val mensage:String)
